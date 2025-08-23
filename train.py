cde5e4016004146364bb1def7b928015177e567b
Ryanfzhang <mafzhang@ust.hk>
2025-08-23 11:33:03 +0800

    slurm

--- train.py
+++ train.py
@@ -56,13 +56,13 @@ lr_scheduler = get_cosine_schedule_with_warmup(
     num_training_steps=len(train_dloader) * args.train_epochs,
 )
 
-# accelerator = Accelerator()
-# device = accelerator.device
-# train_dloader = accelerator.prepare_data_loader(train_dloader)
-# test_dloader = accelerator.prepare_data_loader(test_dloader)
-# model = accelerator.prepare_model(model)
-# optimizer = accelerator.prepare_optimizer(optimizer)
-# lr_scheduler = accelerator.prepare_scheduler(lr_scheduler)
+accelerator = Accelerator()
+device = accelerator.device
+train_dloader = accelerator.prepare_data_loader(train_dloader)
+test_dloader = accelerator.prepare_data_loader(test_dloader)
+model = accelerator.prepare_model(model)
+optimizer = accelerator.prepare_optimizer(optimizer)
+lr_scheduler = accelerator.prepare_scheduler(lr_scheduler)
 
 best_mse_sst, best_mse_salt = 100, 100
 
@@ -76,8 +76,8 @@ for epoch in range(args.train_epochs):
     train_loss = AverageMeter()
     model.train()
     epoch_time = time.time()
-    # for i, (x_phy, x_bio, time, land_mask, _) in tqdm(enumerate(train_dloader), total=len(train_dloader), disable=(not accelerator.is_local_main_process)):
-    for i, (x_phy, x_bio, time, land_mask, _, _, _) in tqdm(enumerate(train_dloader), total=len(train_dloader)):
+    for i, (x_phy, x_bio, time, land_mask, _, _, _) in tqdm(enumerate(train_dloader), total=len(train_dloader), disable=(not accelerator.is_local_main_process)):
+    # for i, (x_phy, x_bio, time, land_mask, _, _, _) in tqdm(enumerate(train_dloader), total=len(train_dloader)):
         x_phy, x_bio, time, land_mask = x_phy.to(device), x_bio.to(device), time.to(device), land_mask.to(device)
         x_bio_recon_f_phy, x_bio_recon_f_bio, x_phy_latent, x_bio_latent = model(x_phy, x_bio, time)
 
@@ -96,31 +96,31 @@ for epoch in range(args.train_epochs):
 
     train_loss.reset()
 
-    # if epoch%10==0: 
-    #     with torch.no_grad():
-    #         recon_f_bio_mse_list, recon_f_phy_mse_list = [], []
-    #         for i, (x_phy, x_bio, time, land_mask, bio_mean, bio_std, _) in tqdm(enumerate(test_dloader), total=len(test_dloader), disable=(not accelerator.is_local_main_process)):
-    #             x_bio_recon_f_phy, x_bio_recon_f_bio, x_phy_latent, x_bio_latent = model(x_phy, x_bio, time)
-    #             land_mask = land_mask[:,None, None, ...]
+    if epoch%10==0: 
+        with torch.no_grad():
+            recon_f_bio_mse_list, recon_f_phy_mse_list = [], []
+            for i, (x_phy, x_bio, time, land_mask, bio_mean, bio_std, _) in tqdm(enumerate(test_dloader), total=len(test_dloader), disable=(not accelerator.is_local_main_process)):
+                x_bio_recon_f_phy, x_bio_recon_f_bio, x_phy_latent, x_bio_latent = model(x_phy, x_bio, time)
+                land_mask = land_mask[:,None, None, ...]
 
-    #             # denormalize
-    #             x_bio_recon_f_phy = x_bio_recon_f_phy * bio_std[...,None, None] + bio_mean[..., None, None]
-    #             x_bio_recon_f_bio= x_bio_recon_f_bio * bio_std[...,None, None] + bio_mean[..., None, None]
+                # denormalize
+                x_bio_recon_f_phy = x_bio_recon_f_phy * bio_std[...,None, None] + bio_mean[..., None, None]
+                x_bio_recon_f_bio= x_bio_recon_f_bio * bio_std[...,None, None] + bio_mean[..., None, None]
 
-    #             recon_f_phy= criteria(x_bio_recon_f_phy, x_bio)
-    #             recon_f_phy_mse = (land_mask * recon_f_phy).sum([3,4])/ land_mask.float().sum([3,4])
+                recon_f_phy= criteria(x_bio_recon_f_phy, x_bio)
+                recon_f_phy_mse = (land_mask * recon_f_phy).sum([3,4])/ land_mask.float().sum([3,4])
 
-    #             recon_f_bio= criteria(x_bio_recon_f_bio, x_bio)
-    #             recon_f_bio_mse = (land_mask * recon_f_bio).sum([3,4])/ land_mask.float().sum([3,4])
+                recon_f_bio= criteria(x_bio_recon_f_bio, x_bio)
+                recon_f_bio_mse = (land_mask * recon_f_bio).sum([3,4])/ land_mask.float().sum([3,4])
 
-    #             recon_f_phy_mse = accelerator.gather(recon_f_phy_mse)
-    #             recon_f_bio_mse = accelerator.gather(recon_f_bio_mse)
-    #             recon_f_bio_mse_list.append(recon_f_bio_mse.flatten(0,1).detach().cpu().numpy())
-    #             recon_f_phy_mse_list.append(recon_f_phy_mse.flatten(0,1).detach().cpu().numpy())
+                recon_f_phy_mse = accelerator.gather(recon_f_phy_mse)
+                recon_f_bio_mse = accelerator.gather(recon_f_bio_mse)
+                recon_f_bio_mse_list.append(recon_f_bio_mse.flatten(0,1).detach().cpu().numpy())
+                recon_f_phy_mse_list.append(recon_f_phy_mse.flatten(0,1).detach().cpu().numpy())
             
-    #         mean_recon_f_bio_mse = np.concatenate(recon_f_bio_mse_list, axis=0).mean(0)
-    #         mean_recon_f_phy_mse = np.concatenate(recon_f_phy_mse_list, axis=0).mean(0)
+            mean_recon_f_bio_mse = np.concatenate(recon_f_bio_mse_list, axis=0).mean(0)
+            mean_recon_f_phy_mse = np.concatenate(recon_f_phy_mse_list, axis=0).mean(0)
 
-    #         accelerator.print("Test" + "*"*48)
-    #         accelerator.print(mean_recon_f_bio_mse)
-    #         accelerator.print(mean_recon_f_phy_mse)
+            accelerator.print("Test" + "*"*48)
+            accelerator.print(mean_recon_f_bio_mse)
+            accelerator.print(mean_recon_f_phy_mse)
